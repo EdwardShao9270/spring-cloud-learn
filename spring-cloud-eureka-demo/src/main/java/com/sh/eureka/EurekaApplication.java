e7dd784d2db3378c1d1acfea37ad0bc78ef92690
bjshaohang <bjshaohang@corp.netease.com>
2019-04-03 20:59:45 +0800

    提交代码

--- spring-cloud-eureka-demo/src/main/java/com/sh/eureka/EurekaApplication.java
+++ spring-cloud-eureka-demo/src/main/java/com/sh/eureka/EurekaApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
+ * 测试git提交代码
  * @author bjshaohang
  */
 @SpringBootApplication
